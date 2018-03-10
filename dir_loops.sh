a81c34c10082b3e29be57cbc271c787c1d31fc7a
acorpuz <angel.corpuz.jr@gmail.com>
2018-03-10 16:44:29 +0100

    Aggiunta pulizia di righe in eccesso dai file di dati
    Creazione ed aggiunta al repo della dir di logs, aggoiunta codice che la crea se non esiste

--- dir_loops.sh
+++ dir_loops.sh
@@ -27,6 +27,11 @@ run_log_name="logs/run_$(date +"%Y_%m_%d-%T").log"
 
 echo "Script running in ${work_dir}" | tee -a "$run_log_name"
 echo "Using csv directory: ${csv_dir}" | tee -a "$run_log_name"
+echo "Checking log dir..." | tee -a "$run_log_name"
+if [ ! -e ./logs ]; then
+    mkdir ./logs
+    chown 775 ./logs
+fi
 
 for i in $csv_dir/*.csv; do
 	# estraggo il nome base del file e lo uso per personalizzare 
@@ -50,13 +55,25 @@ for i in $csv_dir/*.csv; do
 	cp ./template.cfg "$conf_file"
 	sed -i "s/XXX/${filename}/g" "$conf_file"
 	
+	echo "Cleaning up data file ${csvfile} ..." | tee -a "$run_log_name"
+    # get max value in file
+    maxval=$(tail -n1 "${csvfile}" | cut -d , -f 1 )
+    # count occurances, ideally only 2
+    last_lines=$(grep -c "$maxval" "${csvfile}")
+    if [ "$last_lines" -gt 2 ]; then
+        echo "Extra lines foud in ${csvfile}, deleting lines ..." | tee -a "$run_log_name"
+        # delete lines matching the pattern 
+        grep -v "$maxval,.,up" "${csvfile}" > "${csvfile}.tmp"
+        mv "${csvfile}.tmp" "${csvfile}"
+    fi
+
 	echo "Running program...." | tee -a "$run_log_name"
 	echo -e "\tUsing cfg file" "$conf_file" | tee -a "$run_log_name"
 	echo -e "\tLogging all data in logs/" | tee -a "$run_log_name"
 	
 	# chiamata al programma, levare il commento
 #	./3incomms SIM -np "$csvfile" --config_file "$conf_file"
-	echo "Done simulation for ${filename}" | tee -a "$run_log_name"
+	echo "Done simulation for ${csvfile}" | tee -a "$run_log_name"
     echo "Waiting 5 seconds ..." | tee -a "$run_log_name"
     sleep 5
 	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++" | tee -a "$run_log_name"
